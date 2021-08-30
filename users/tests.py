019303c6ca20a01f27b70169325880c08e5931e7
sayanjitdas <sayanjitgit@gmail.com>
2021-08-30 12:16:54 +0530

    commit to registration user

--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,9 @@
+from users.views import SignupPageView
+from users.forms import CustomUserCreationForm
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.test.utils import get_runner
+from django.urls import reverse,resolve
 # Create your tests here.
 
 class CustomUserTests(TestCase):
@@ -28,3 +31,28 @@ class CustomUserTests(TestCase):
         self.assertEqual(user.username,"superadmin")
         self.assertEqual(user.email,"superadmin@email.com")
         self.assertTrue(user.is_superuser)
+
+class SignupPageTests(TestCase):
+
+    def setUp(self):
+
+        self.url = reverse('signup')
+        self.response = self.client.get(self.url)
+
+    def test_signup_url_status(self):
+        self.assertEqual(self.response.status_code,200)
+
+    def test_signup_template_check(self):
+        self.assertTemplateUsed(self.response,'signup.html')
+    
+    def test_signup_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form,CustomUserCreationForm)
+        self.assertContains(self.response,'csrfmiddlewaretoken') 
+
+    def test_signup_view(self):
+        view = resolve(self.url)
+        self.assertEqual(
+            view.func.__name__,
+            SignupPageView.as_view().__name__
+        )       
